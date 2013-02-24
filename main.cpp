cf22853c64f0c1cb57cc5b2b8e18e25287f49b0c
morslilleole <oleveg@gmail.com>
2013-02-24 20:01:06 +0100

    Implemented own code for creating surface, remove unused code

--- main.cpp
+++ main.cpp
@@ -8,60 +8,51 @@ char* doTracing();
 vector2d square_tl( 40, 40 );
 vector2d square_br( 60, 60);
 
-void drawArray( char* pixels, SDL_Surface* screen )
-{
-	SDL_Surface* hello = NULL; 
 
-	//Load image 
-	hello = SDL_LoadBMP( "hello.bmp" );
-
-	
-	hello->pixels = pixels;
-	std::cout << "w : " << hello->w << " h : " << hello->h << " format : " << hello->format << std::endl;
-	std::cout << "data[0] : " << (int) pixels[0]  << std::endl;
-
-	//Apply image to screen 
-	SDL_BlitSurface( hello, NULL, screen, NULL ); 
-
-	//Update Screen 
-	SDL_Flip( screen ); 
-
-
-	//Free the loaded image 
-	SDL_FreeSurface( hello ); 
-}
 int main( int argc, char* args[] ) { 
 
 	//The images 
 	SDL_Surface* screen = NULL;
+	SDL_Surface* traced  = NULL;
 
 	//Start SDL 
 	SDL_Init( SDL_INIT_EVERYTHING ); 
 
 	//Set up screen 
-	screen = SDL_SetVideoMode( 640, 480, 32, SDL_SWSURFACE ); 
+	screen = SDL_SetVideoMode( 640, 480, 32, SDL_SWSURFACE );
 
-	drawArray( doTracing(), screen );
+	char* pixels = doTracing();
+	// Create square surfeace
+	traced = SDL_CreateRGBSurfaceFrom( pixels, 640, 480, 32, 640 * 4, 0, 0, 0, 0);//0xFF000000, 0x00FF0000, 0x0000FF00, 0x000000FF );
 
-	std::cin.ignore();
+	//Apply image to screen 
+	SDL_BlitSurface( traced, NULL, screen, NULL ); 
+
+	//Update Screen 
+	SDL_Flip( screen ); 
+
+	//Free the loaded image 
+	SDL_FreeSurface( traced ); 
+
 
 	//Pause 
-	//SDL_Delay( 2000 );
+	SDL_Delay( 2000 );
 
 	//Quit SDL 
 	SDL_Quit(); 
 
-	//delete[] pixels;
+	delete[] pixels;
 	
 	return 0; 
 }
 
 char* doTracing()
 {
+	// Create pixel array
+	char* pixels = new char[640*480*4];
 
-	char* pixels = new char[640*480*3];
-
-	memset( pixels, 0, 640 * 480 * 3);
+	// Set all values to 0 ( black )
+	memset( pixels, 0, 640 * 480 * 4);
 
 	for ( int x = 0; x < 640 ; ++x )
 	{
@@ -73,10 +64,10 @@ char* doTracing()
 				&& ( y >= square_tl.y && y <= square_br.y)
 			   )
 			{
-				std::cout << "intersection : " << x << " , " << y << std::endl;
-				memset( &pixels[ (  ( x + (  y * 640  )  ) *  3 ) + 0 ] , 255, 1 ); // Red
-				//memset( &pixels[ (  ( x + (  y * 640  )  ) *  3 ) + 1 ] , 255, 1 ); // Green
-				memset( &pixels[ (  ( x + (  y * 640  )  ) *  3 ) + 2 ] , 255, 1 ); // Blue
+				// Set pixel ( x, y ) to 255, 255, 255 ( white )
+				memset( &pixels[ (  ( x + (  y * 640  )  ) *  4 ) + 0 ] , 255, 1 ); // Red
+				memset( &pixels[ (  ( x + (  y * 640  )  ) *  4 ) + 1 ] , 255, 1 ); // Green
+				memset( &pixels[ (  ( x + (  y * 640  )  ) *  4 ) + 2 ] , 255, 1 ); // Blue
 			}
 
 		}
